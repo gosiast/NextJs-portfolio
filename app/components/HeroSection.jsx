971a07c9b6f48e9ab4154c85470ef289d268f0b6
Malgorzata Stano <stano.malgorzata@gmail.com>
2023-12-17 15:50:16 +0100

    styled the header in the HeroSection

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 const HeroSection = () => {
-    return <section>
-        <h1 className="text-white">Portfolio Website</h1>
+    return (<section>
+        <h1 className="text-white mb-4 text-6xl font-extrabold">Hello, I'm Gosia</h1>
+   <p className="text-[#ADB7BE]">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+   </p>
     </section>
-};
+)};
 
 export default HeroSection;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import HeroSection from './components/HeroSection'
 
 export default function Home() {
   return (
-    <main className="">
-     <h1>Portfolio Website</h1>
-    </main>
+    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
+<HeroSection/>    
+</main>
   )
 }
