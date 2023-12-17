bc9ef17c22539fcb2b4e13d397b064d3b64a8ada
Malgorzata Stano <stano.malgorzata@gmail.com>
2023-12-17 16:04:36 +0100

    updated the styling of the HeroSection

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -2,10 +2,17 @@ import React from "react";
 
 const HeroSection = () => {
     return (<section>
-        <h1 className="text-white mb-4 text-6xl font-extrabold">Hello, I'm Gosia</h1>
-   <p className="text-[#ADB7BE]">
+        <div className="grid grid-cols-1 lg:grid-cols-12">
+            <div className="col-span-7 place-self-center"> 
+        <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I'm Gosia</h1>
+   <p className="text-[#ADB7BE] text-lg lg:text-xl">
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
    </p>
+   </div>
+   </div>
+   <div className="col-span-5">
+
+   </div>
     </section>
 )};
 
