e7629d53729fa4322d9e65424cdf0bc1f1f5e898
Malgorzata Stano <stano.malgorzata@gmail.com>
2023-12-18 09:28:48 +0100

    added lg:flex to the section in order to fix issue with photo

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import Image from "next/image";
 
 const HeroSection = () => {
-    return (<section>
+    return (
+    <section className="lg:flex">
         <div className="grid grid-cols-1 lg:grid-cols-12">
             <div className="col-span-7 place-self-center"> 
         <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I'm Gosia</h1>
