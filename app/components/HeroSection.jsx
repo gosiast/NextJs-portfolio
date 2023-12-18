a961760d06f1d321cb17a86ac1438b37b13913b1
Malgorzata Stano <stano.malgorzata@gmail.com>
2023-12-18 09:36:00 +0100

    added styling to buttons on lg and sm devices

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -4,15 +4,15 @@ import Image from "next/image";
 const HeroSection = () => {
     return (
     <section className="lg:flex">
-        <div className="grid grid-cols-1 lg:grid-cols-12">
-            <div className="col-span-7 place-self-center"> 
-        <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I'm Gosia</h1>
-   <p className="text-[#ADB7BE] text-lg lg:text-xl mb-6">
+        <div className="grid grid-cols-1 sm:grid-cols-12">
+            <div className="col-span-7 place-self-center text-center sm:text-left"> 
+        <h1 className="text-white mb-4 text-4xl sm:text:5xl lg:text-6xl font-extrabold">Hello, I'm Gosia</h1>
+   <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6">
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
    </p>
    <div>
-    <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-400 text-black hover:text-white mb-3">Hire Me</button>
-    <button className= "px-6 py-3 rounded-full mr-4 bg-transparent hover:bg-slate-400 text-white hover:text-black border border-white mb-3">Download CV</button>
+    <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-400 text-black hover:text-white mb-3">Hire Me</button>
+    <button className= "px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-transparent hover:bg-slate-400 text-white hover:text-black border border-white mb-3">Download CV</button>
    </div>
    </div>
    </div>
