f0d482a26404e1643bc0eb5b444106ecce56de17
Malgorzata Stano <stano.malgorzata@gmail.com>
2023-12-18 09:19:31 +0100

    added hire me and download cv buttons

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -6,12 +6,16 @@ const HeroSection = () => {
         <div className="grid grid-cols-1 lg:grid-cols-12">
             <div className="col-span-7 place-self-center"> 
         <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I'm Gosia</h1>
-   <p className="text-[#ADB7BE] text-lg lg:text-xl">
+   <p className="text-[#ADB7BE] text-lg lg:text-xl mb-6">
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
    </p>
+   <div>
+    <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-400 text-black hover:text-white">Hire Me</button>
+    <button className= "px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-400 text-black hover:text-white">Download CV</button>
    </div>
    </div>
-   <div className="rounded-full bd-[#181818] w-500 h-400 relative">
+   </div>
+   <div className="col-span-5 place-self-center mt-4 lg:mt-0">
     <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/hero_photo.png"
