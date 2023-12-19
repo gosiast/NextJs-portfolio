c7c8dbac4edd1908dfa665c1a1cfca7bb2b037aa
Malgorzata Stano <stano.malgorzata@gmail.com>
2023-12-19 08:14:14 +0100

    removed irrelevant styling of the animation

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -23,7 +23,6 @@ const HeroSection = () => {
       ]}
       wrapper="span"
       speed={50}
-      style={{ fontSize: '2em', display: 'inline-block' }}
       repeat={Infinity}
     />    
     </h1>
