75abd0889170f7315a0e17f78b3c03a512f29ffe
Malgorzata Stano <stano.malgorzata@gmail.com>
2023-12-20 21:25:48 +0100

    deleted irrelevant navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,57 +1,55 @@
 "use client";
-import React, {useState} from "react";
 import Link from "next/link";
+import React, { useState } from "react";
 import NavLink from "./NavLink";
-import { Bars4Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
-{
+  {
     title: "About",
     path: "#about",
-},
-
-{
+  },
+  {
     title: "Projects",
     path: "#projects",
-},
-   {
+  },
+  {
     title: "Contact",
     path: "#contact",
-   }
-]
+  },
+];
 
-// when useState is false then the icon isn't displayed
 const Navbar = () => {
-    const [navbarOpen, setNavbarOpen] = useState(true);
-    return (
-        <nav className="fixed top-0 left-0 right-0 z-10 bg-[#0b0b0b] bg-opacity-90">
-             <div className="flex flex-wrap items-center justify-between mx-auto px-4"> 
-                <Link href={"/"} className="text-xl md:text-5xl text-white font-semibold pt-5">
-                    LOGO
-                </Link>
-                <div className="mobile-menu block md:hidden">
-{
-    //below there is a condition, what will happen with different buttons,
-    //also button icons need className otherwise they're not visible
-    navbarOpen ? (
-        <button
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  return (
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-80">
+      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+        <Link
+          href={"/"}
+          className="text-2xl md:text-5xl text-white font-semibold"
+        >
+          LOGO
+        </Link>
+        <div className="mobile-menu block md:hidden">
+          {!navbarOpen ? (
+            <button
               onClick={() => setNavbarOpen(true)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-white hover:text-white hover:border-white"
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
             >
-              <Bars4Icon className="h-5 w-5" />
+              <Bars3Icon className="h-5 w-5" />
             </button>
           ) : (
             <button
               onClick={() => setNavbarOpen(false)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-white hover:text-white hover:border-white"
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
             >
               <XMarkIcon className="h-5 w-5" />
             </button>
-    )
-}
-                </div>
-             <div className="menu hidden md:block md:w-auto" id="navbar">
+          )}
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
             {navLinks.map((link, index) => (
               <li key={index}>
@@ -60,10 +58,10 @@ const Navbar = () => {
             ))}
           </ul>
         </div>
-             </div>
+      </div>
       {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
-        </nav>
-    )
-}
+    </nav>
+  );
+};
 
 export default Navbar;
