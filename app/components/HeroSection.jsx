f77950d3448a403049682c2ea8e34d57007a1d44
Malgorzata Stano <stano.malgorzata@gmail.com>
2023-12-20 09:22:36 +0100

    updated image animation and problems with styling of H1 animation

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -7,27 +7,28 @@ const HeroSection = () => {
     return (
     <section className="lg:flex">
         <div className="grid grid-cols-1 sm:grid-cols-12">
-            <div className="col-span-7 place-self-center text-center sm:text-left"> 
-        <h1 className="text-white mb-4 text-4xl sm:text:5xl lg:text-6xl font-extrabold">
+            <div className="col-span-8 place-self-center text-center sm:text-left"> 
+        <h1 className="text-white mb-4 text-4xl sm:text:5xl lg:text-5xl font-extrabold">
            <span className="text-transparent bg-clip-text bg-gradient-to-br from-pink-700 via-purple-400 to-blue-400 hover:bg-slate-400">
             Hello, I'm {" "} 
             </span> 
+            <br/>
         <TypeAnimation
       sequence={[
         'Gosia',
-        2000, 
+        1500, 
         'Frontend Developer',
-        2000,
+        1500,
         'Lifelong Learner',
-        2000,
+        1500,
       ]}
       wrapper="span"
       speed={50}
       repeat={Infinity}
-    />    
+    />
     </h1>
    <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6">
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
    </p>
    <div>
     <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-pink-700 via-purple-400 to-blue-400 hover:bg-slate-400 text-white hover:text-black mb-3">
@@ -42,13 +43,13 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
    </div>
    </div>
    <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-    <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+    <div className="w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/hero_photo.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
+              width={400}
+              height={400}
             />
           </div>
    </div>
