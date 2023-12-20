aee5a446920d94b2377d342f92176972c9e1c8c8
Malgorzata Stano <stano.malgorzata@gmail.com>
2023-12-20 09:27:48 +0100

    created a navbar component

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+    return (
+        <nav>
+             <div> 
+                <Link href={"/"}>LOGO</Link>
+             </div>
+        </nav>
+       
+    )
+}
+
+export default Navbar
