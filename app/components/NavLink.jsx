5d6f66bbbe1c59ff6fcd4e701e30f169d06e948d
Malgorzata Stano <stano.malgorzata@gmail.com>
2023-12-20 10:01:54 +0100

    added NavLinks, need styling

--- app/components/NavLink.jsx
+++ app/components/NavLink.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Link from "next/link";
 
 const NavLink = ( {href, title}) => {
     return (
 <Link href={href}
-className="text-5xl text-white font-semibold">
+className="block py-2 pl-3 pr-4 text-4xl text-white">
 {title}
 </Link>
     )
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,16 +1,39 @@
 import React from "react";
 import Link from "next/link";
+import NavLink from "./NAvLink";
+
+
+const NavLinks = [
+{
+    title: "About",
+    path: "#about",
+},
+
+{
+    title: "Projects",
+    path: "#projects",
+},
+   {
+    title: "Contact",
+    path: "#contact",
+   }
+]
 
 const Navbar = () => {
     return (
         <nav>
              <div className="flex flex-wrap items-center justify-between mx-auto p-8"> 
-                <Link href={"/"} className="text-5xl text-white font-semibold">LOGO</Link>
-             <div className="menu hidden md:block md:w-auto" id="navbar">
+                <Link href={"/"} className="text-xl text-white font-semibold">LOGO</Link>
+             <div className="menu block md:w-auto" id="navbar">
                <ul>
-                    <li>
-                        <Link href={"#about"} className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white">About</Link>
-                    </li>
+                {
+                     NavLinks.map((link, index) => (
+                        <li key={index}>  
+                        <NavLink href={link.path} title={link.title} />
+                        </li>
+                     )     
+                     )
+                }           
                 </ul> 
 
              </div>
