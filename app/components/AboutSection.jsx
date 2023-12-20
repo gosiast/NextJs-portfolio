43b1492090a0eda9f79cb1e42edac4c4ed61201f
Malgorzata Stano <stano.malgorzata@gmail.com>
2023-12-20 21:48:39 +0100

    created a new component AboutSection

---
+++ app/components/AboutSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AboutSection = () => {
+    return (
+<section className="text-white"> 
+<div>
+    <h2>About Me</h2>   
+    <p>My name is Małgorzata, but everyone calls me Gosia. I was born in Poland 🇵🇱 but I've been living abroad over the past few years. 
+        Currently, Spain 🇪🇸 has become my home. Since 2019 I've been working as an English Teacher. In 2021 I've decided to shift my career and I fell in love with coding. 
+        It's fascinating, challenging but incredibly rewarding. I would like to continue to grow as a Frontend Developer and learn more about the IT sector.</p> 
+    </div>
+    </section>
+)
+}
+
+export default AboutSection;
