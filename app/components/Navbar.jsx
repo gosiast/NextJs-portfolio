cb6b879dfe209ed304d75dceaa408f2563408630
Malgorzata Stano <stano.malgorzata@gmail.com>
2023-12-20 19:36:52 +0100

    fixed the navbar so it doesn't overlap

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -21,11 +21,11 @@ const NavLinks = [
 
 const Navbar = () => {
     return (
-        <nav className="fixed top-0 left-0 right-0">
-             <div className="flex flex-wrap items-center justify-between mx-auto p-8"> 
+        <nav className="fixed top-0 left-0 right-0 z-10 bg-[#0b0b0b] bg-opacity-90">
+             <div className="flex flex-wrap items-center justify-between mx-auto"> 
                 <Link href={"/"} className="text-xl md:text-5xl text-white font-semibold">LOGO</Link>
              <div className="menu block md:w-auto" id="navbar">
-               <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+               <ul className="flex p-8 md:p-0 md:flex-row md:space-x-8 mt-0">
                 {
                      NavLinks.map((link, index) => (
                         <li key={index}>  
