05b9beb8ac24f44af8134852f554740368a4df71
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2023-12-25 08:58:26 +0100

    added icons to the links in projects

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CodeBracketIcon, GlobeAlt } from "@heroicons/react/24/outline";
+import { CodeBracketIcon, GlobeAltIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
 //a few props: imgUrl, title,
@@ -19,9 +19,15 @@ const ProjectCard = ({ imgUrl, title, description }) => {
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link
             href="/"
-            className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            className="h-14 w-14 mr-4 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
             <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+          </Link>
+          <Link
+            href="/"
+            className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+          >
+            <GlobeAltIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
           </Link>
         </div>
       </div>
