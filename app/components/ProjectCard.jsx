6836cb5d10f4921a73dc0e16ce154a5f046b0123
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2023-12-25 09:07:33 +0100

    updated links to my projcts

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 //a few props: imgUrl, title,
 // className overlay is when we hover the button
 // when i forgot relative group in the parent element, the hover didn't work
-const ProjectCard = ({ imgUrl, title, description }) => {
+const ProjectCard = ({ imgUrl, title, description, githubUrl, websiteUrl }) => {
   return (
     <div>
       <div
@@ -18,13 +18,13 @@ const ProjectCard = ({ imgUrl, title, description }) => {
       >
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link
-            href="/"
+            href={githubUrl}
             className="h-14 w-14 mr-4 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
             <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
           </Link>
           <Link
-            href="/"
+            href={websiteUrl}
             className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
             <GlobeAltIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -8,6 +8,8 @@ const projectsData = [
     description:
       "Built with React, advanced HTML, CSS, Javascript and Boostrap. Using real data and API. Final project of the SheCodes React course",
     image: "/images/projects/react_weather_app.png",
+    githubUrl: "https://github.com/gosiast/react-dictionary-app",
+    websiteUrl: "https://my-first-react-dictionary-ms.netlify.app/",
   },
   {
     id: 2,
@@ -15,6 +17,8 @@ const projectsData = [
     description:
       "Built with React, advanced HTML, CSS, Javascript and Boostrap. Using real data and API. Final project of the SheCodes React course",
     image: "/images/projects/react_dictionary_app.png",
+    githubUrl: "https://github.com/gosiast/final-weather-app",
+    websiteUrl: "https://final-weather-app-react.netlify.app/",
   },
   {
     id: 3,
@@ -22,6 +26,8 @@ const projectsData = [
     description:
       "Built with advanced HTML, CSS, Javascript and Boostrap. Using real data and API. Final project of the SheCodes Plus course",
     image: "/images/projects/weather_app_photo.png",
+    githubUrl: "https://github.com/gosiast/vanilla-weather-app/tree/main",
+    websiteUrl: "https://sad-darwin-5ac17f.netlify.app/",
   },
   {
     id: 4,
@@ -29,6 +35,8 @@ const projectsData = [
     description:
       "My First Portfolio build with HTMl and CSS. In this project I learnt how to build responsive websites. Final project of the SheCodes Responsive course",
     image: "/images/projects/first_portfolio.png",
+    githubUrl: "",
+    websiteUrl: "",
   },
   {
     id: 5,
@@ -36,13 +44,17 @@ const projectsData = [
     description:
       "Built with HTML, CSS and Javascript. Final project of the SheCodes Basic course",
     image: "/images/projects/first_website.png",
+    githubUrl: "",
+    websiteUrl: "https://malgorzatastanoportfolio.netlify.app/",
   },
 ];
 
 const ProjectsSection = () => {
   return (
     <>
-      <h2 className="text-center text-2xl font-bold text-white mt-4 mb-5">My Projects</h2>
+      <h2 className="text-center text-2xl font-bold text-white mt-4 mb-5">
+        My Projects
+      </h2>
       <div className="grid md:grid-cols-2 gap-8 md:gap-12">
         {projectsData.map((project) => (
           <ProjectCard
