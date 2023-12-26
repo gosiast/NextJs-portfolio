fc3aed9af3ecf4719fe7f715b3f7692dd5d74ab4
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2023-12-26 11:57:51 +0100

    updated social media icons

--- app/components/ContactMe.jsx
+++ app/components/ContactMe.jsx
@@ -7,8 +7,8 @@ import Image from "next/image";
 
 const ContactMe = () => {
   return (
-    <section className="grid md:grid-cols2 my-12 md:my-12 py-24 gap-4">
-      <div>
+    <section className="grid grid-cols-1 sm:grid-cols-12">
+      <div className="col-span-8 place-self-center text-center sm:text-left">
         <h5 className="text-xl font-bold text-white my-2">Let's Connect!</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           I am currently looking for full-time remote (or hybrid from Barcelona)
@@ -18,16 +18,23 @@ const ContactMe = () => {
           <Link href="https://github.com/gosiast">
             <Image src={GithubIcon} alt="GitHub Icon" />
           </Link>
-        </div>
-        <div className="socials flex flex-row gap-2">
           <Link href="https://www.linkedin.com/in/malgorzata-stano/">
             <Image src={LinkedIcon} alt="LinkedIn Icon" />
           </Link>
-        </div>
-        <div className="socials flex flex-row gap-2">
           <Link href="mailto:stano.malgorzata@gmail.com" target="_blank">
-            <Image src={EmailIcon} alt="Email Icon" />
+            <Image src={EmailIcon} alt="Email Icon" className="text:white" />
           </Link>
+        </div>
+      </div>
+      <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+        <div className="w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <Image
+            src="/images/oyster_image.png"
+            alt="animation of me, sitting with a computer and world map in the background"
+            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+            width={400}
+            height={400}
+          />
         </div>
       </div>
     </section>
--- app/page.js
+++ app/page.js
@@ -1,18 +1,20 @@
-import Image from 'next/image'
-import HeroSection from './components/HeroSection'
-import Navbar from './components/Navbar'
-import AboutSection from './components/AboutSection'
-import ProjectsSection from './components/ProjectsSection'
+import Image from "next/image";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
+import ContactMe from "./components/ContactMe";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Navbar/>
+      <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
-        <HeroSection/>  
-        <AboutSection/>  
-        <ProjectsSection/>
+        <HeroSection />
+        <AboutSection />
+        <ProjectsSection />
+        <ContactMe />
       </div>
-</main>
-  )
+    </main>
+  );
 }
