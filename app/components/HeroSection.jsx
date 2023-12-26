87c08faaa7036d2b0035433137b1662d91b3b0ce
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2023-12-26 11:26:58 +0100

    updated the hero photo

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -49,7 +49,7 @@ const HeroSection = () => {
       <div className="col-span-5 place-self-center mt-4 lg:mt-0">
         <div className="w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
           <Image
-            src="/images/me_animation.png"
+            src="/images/hero_photo.png"
             alt="animation of me, sitting with a computer and world map in the background"
             className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
             width={400}
