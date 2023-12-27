2efb56e0e72c84561608d8c4a3a6d8035091fabe
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2023-12-27 21:30:17 +0100

    updated the hire me button with mailto

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
     <section className="lg:flex px-0">
       <div className="display-block grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-8 place-self-center text-center sm:text-left">
-          <h1 className="text-white mb-4 text-4xl sm:text:5xl lg:text-5xl font-extrabold">
+          <h1 className="text-white mb-8 text-4xl sm:text:5xl lg:text-5xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-br from-pink-700 via-purple-400 to-blue-400 hover:bg-slate-400">
               Hello, I'm{" "}
             </span>
@@ -31,16 +31,18 @@ const HeroSection = () => {
             />
             <br />
           </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-m lg:text-l mb-8">
+          <p className="text-[#ADB7BE] text-base sm:text-m lg:text-xl mb-8">
             Welcome to my digital playground! I&apos;m on a journey to craft
             seamless and captivating web experiences. Dive in, explore my
             creations, and get to know the code that brings ideas to life.
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-pink-700 via-purple-400 to-blue-400 hover:bg-slate-400 text-white hover:text-black mb-3">
-              Hire Me
+            <button className="px-7 py-4 w-full sm:w-fit rounded-full mr-5 bg-gradient-to-br from-pink-700 via-purple-400 to-blue-400 hover:bg-slate-400 text-white hover:text-black mb-3">
+              <a href="mailto:stano.malgorzata@gmail.com" className="href">
+                Hire Me
+              </a>
             </button>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-transparent from-pink-700 via-purple-400 to-blue-400 hover:bg-slate-200 text-white hover:text-black border mb-3">
+            <button className="px-7 py-3 w-full sm:w-fit rounded-full mr-5 bg-transparent from-pink-700 via-purple-400 to-blue-400 hover:bg-slate-200 text-white hover:text-black border mb-3">
               <span>
                 <a href="/Malgorzata_Stano_Resume.pdf" target="_blank">
                   Download CV
