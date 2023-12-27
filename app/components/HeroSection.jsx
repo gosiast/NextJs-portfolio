74b09ee1559c3cf90df9bf9d78b65a90caee0e7f
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2023-12-27 22:04:43 +0100

    updated the issue with double quotes in the HeroSection

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -13,7 +13,7 @@ const HeroSection = () => {
         <div className="col-span-8 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-8 text-4xl sm:text:5xl lg:text-5xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-br from-pink-700 via-purple-400 to-blue-400 hover:bg-slate-400">
-              Hello, I'm
+              Hello, I&apos;m
             </span>
             <br />
             <TypeAnimation
