c6d1337806777c53b9a8d950fe8b177457d1cbf8
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-01-04 08:56:47 +0100

    changed the icon in the Contact Me part

--- app/components/ContactMe.jsx
+++ app/components/ContactMe.jsx
@@ -39,11 +39,11 @@ const ContactMe = () => {
       <div className="mt-4 md:mt-0 text-center md:text-left">
         <div className="w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
           <Image
-            src="/images/me_animation.png"
+            src="/images/oyster_image.png"
             alt="animation of me, sitting with a computer and world map in the background"
             className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            width={400}
-            height={400}
+            width={500}
+            height={500}
           />
         </div>
       </div>
